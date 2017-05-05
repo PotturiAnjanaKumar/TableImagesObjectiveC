8375dfe04154fce37b9137d840225dc801372730
Apple <apple@apples-macbook-pro.local>
2017-05-05 11:19:29 +0530

    Initial Commit

---
+++ TableImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableImages
+//
+//  Created by Apple on 05/05/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
